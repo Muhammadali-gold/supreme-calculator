3123865b894293a0be06088d7ce328a092134e5b
Muhammad <55012876+Muhammadali-gold@users.noreply.github.com>
2022-02-28 15:37:23 +0500

    add greeting api

--- src/main/java/com/dreamsoft/supremecalculator/controller/HomeController.java
+++ src/main/java/com/dreamsoft/supremecalculator/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.dreamsoft.supremecalculator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-//@Controller
-//public class HomeController {
-//
-//
-//}
+@RestController
+public class HomeController {
+
+   @RequestMapping(value="/greeting",method = RequestMethod.GET)
+   public String getGreeting(){
+       return "hello heroku";
+   }
+}
