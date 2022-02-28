e5400fc4e7879eb5fd8950116f3d2df519310eab
Muhammad <55012876+Muhammadali-gold@users.noreply.github.com>
2022-02-28 05:19:16 +0500

    make ui for calculator (not finished)

---
+++ src/main/java/com/dreamsoft/supremecalculator/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.dreamsoft.supremecalculator.controller;
+
+import org.springframework.stereotype.Controller;
+
+//@Controller
+//public class HomeController {
+//
+//
+//}
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,12 @@
+function test(){
+    console.log('hello');
+}
+
+
+function btnClicked(value){
+    console.log('write',value);
+}
+
+function operation(operator){
+    console.log('operation',operator);
+}
